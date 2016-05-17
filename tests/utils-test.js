fcd2350bb8f7feefd02a8f7b96dd19b02530bb97
Guilherme Oenning <oenning.ti@gmail.com>
2016-05-17 17:37:40 -0300

    refactoring and adding more nodejs versions to travis

--- tests/utils-test.js
+++ tests/utils-test.js
@@ -3,24 +3,6 @@ var utils = require('../lib/utils.js');
 
 describe('Utils Tests', function() {
   [
-    [ 'apple', 'ap' ],
-    [ 'Apple', 'Ap' ]
-  ].forEach((testCase) => {
-    it(`${testCase[0]} should start with ${testCase[1]}`, function() {
-      expect(testCase[0].startsWith(testCase[1])).to.be.true;
-    });
-  });
-
-  [
-    [ 'Apple', 'ap' ],
-    [ 'apple', 'at' ]
-  ].forEach((testCase) => {
-    it(`${testCase[0]} should not start with ${testCase[1]}`, function() {
-      expect(testCase[0].startsWith(testCase[1])).to.be.false;
-    });
-  });
-
-  [
     [ 'a=1&b=2', 'a=1&amp;b=2' ],
     [ '"Hello"', '&quot;Hello&quot;' ],
     [ `'World'`, '&#39;World&#39;' ],
