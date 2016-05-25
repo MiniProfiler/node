545f1bdc70523b1b83abc5fb1fe55ae39637a8dd
Guilherme Oenning <oenning.ti@gmail.com>
2016-05-25 12:07:09 -0300

    fix lint

--- tests/servers/index.js
+++ tests/servers/index.js
@@ -3,20 +3,20 @@ var frameworks = [ 'koa', 'express' ];
 var all = [ ];
 
 for (var fw of frameworks) {
-  server = require(`./${fw}`)
+  var server = require(`./${fw}`);
   server.framework = fw;
 
   server.get = (path, cb) => {
     request.get(`http://localhost:8080${path}`, (err, response, body) => {
       cb(err, response, body);
     });
-  }
+  };
 
   server.post = (path, params, cb) => {
     request.post({url: `http://localhost:8080${path}`, form: params }, (err, response, body) => {
       cb(err, response, body);
     });
-  }
+  };
 
   all.push(server);
 }
